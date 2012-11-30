8c28d3b43d8835baf4f247920f9cb6865d828923
Chris Borrowdale <chris@fueled.com>
2012-11-30 10:39:56 +0000

    Initial
    Signed-off-by: Chris Borrowdale <chris@fueled.com>

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>121212 Concert</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+
+    <!--[if IE]>
+        <script type="text/javascript" src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/style.css" type="text/css" />
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+
+    <!--[if (gte IE 6)&(lte IE 8)]>
+        <script type="text/javascript" src="<?php bloginfo('template_url') ?>/js/selectivizr.min.js"></script>
+        <script type="text/javascript" src="<?php bloginfo('template_url') ?>/js/corner.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+
+    <header role="banner">
+        <div class="wrapper cf">
+
+
+                <nav class="g two-fifths" role="navigation">
+                    <ul class="nav">
+                        <li><a href="#" class="icon-facebook"><span aria-hidden="true">Facebook</span></a></li>
+                        <li><a href="#" class="icon-twitter"><span aria-hidden="true">Twitter</span></a></li>
+                        <li id="donate-btn"><a class="btn donate"><span class="icon-robin"></span>Donate</a></li>
+                    </ul>
+                </nav>
+
+        </div>
+    </header>
---
+++ js/main.js
@@ -0,0 +1,135 @@
+
+$(window).ready(function() {
+  $('.timer').countdown("2012/12/12 19:00:00", function(event) {
+    var $this = $(this);
+    switch(event.type) {
+      case "seconds":
+      case "minutes":
+      case "hours":
+      case "days":
+      case "weeks":
+      case "daysLeft":
+        $this.find('span#'+event.type).html(event.value);
+        break;
+      case "finished":
+        $this.hide();
+        break;
+    }
+  });
+
+  $('.videos').roundabout({
+    minOpacity:1,
+    minScale:0.8,
+    enableDrag: true
+  });
+
+  $(".round-next").click(function() {
+      $(".videos").roundabout("animateToNextChild");
+      return false;
+   });
+
+  $(".round-prev").click(function() {
+      $(".videos").roundabout("animateToPreviousChild");
+      return false;
+   });
+
+  $('.images').flexslider({
+     animation: "slide",
+    animationLoop: false,
+    itemWidth: 310,
+    controlNav: false,
+    directionNav: true,
+    slideshow: true,
+    useCSS: false,
+    touch: false
+  });
+
+
+    var vimeoPlayers = jQuery('.featured-img').find('iframe'), player;
+
+    for (var i = 0, length = vimeoPlayers.length; i < length; i++) {
+            player = vimeoPlayers[i];
+            $f(player).addEvent('ready', ready);
+    }
+
+    function addEvent(element, eventName, callback) {
+        if (element.addEventListener) {
+            element.addEventListener(eventName, callback, false)
+        } else {
+            element.attachEvent(eventName, callback, false);
+        }
+    }
+
+    function ready(player_id) {
+        var froogaloop = $f(player_id);
+        froogaloop.addEvent('play', function(data) {
+            jQuery('.featured-img').flexslider("pause");
+        });
+        froogaloop.addEvent('pause', function(data) {
+            jQuery('.featured-img').flexslider("play");
+        });
+    }
+
+    jQuery(".featured-img")
+    .fitVids()
+    .flexslider({
+        animation: "slide",
+        animationLoop: false,
+        smoothHeight: true,
+        useCSS: false,
+        controlNav: false,
+        slideshow:true,
+        touch: false,
+        before: function(slider){
+            if (slider.slides.eq(slider.currentSlide).find('iframe').length !== 0)
+                  $f( slider.slides.eq(slider.currentSlide).find('iframe').attr('id') ).api('pause');
+        }
+    });
+
+
+  $('.robin-hood-stories').flexslider({
+    animation: "slide",
+    controlNav: false,
+    slideshow: false,
+    useCSS: false,
+    touch: false
+  });
+
+  $('.stories').flexslider({
+    animation: "slide",
+    itemWidth: 320,
+    maxItems:3,
+    controlNav: false,
+    slideshow: false,
+    initDelay: 400,
+    useCSS: false,
+    touch: false
+  });
+
+  $('.twitter').flexslider({
+    animation: "slide",
+    directionNav: false,
+    controlNav: true,
+    slideshow: true,
+    initDelay: 2000,
+    controlNav:false,
+    directionNav:false,
+    useCSS: false,
+          pauseOnHover: true,
+          pauseOnAction: true,
+          touch: false
+
+  });
+
+  $(".overlay").fancybox({
+    type: 'iframe'
+  });
+
+  $(".fancybox").fancybox({
+    type: 'image'
+  });
+
+  $(".lightbox-text").fancybox({
+
+  });
+});
