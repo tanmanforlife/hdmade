4dfb38a252b07c8efb2ff98d5064349934b6813f
Chris Borrowdale <chrisborrowdale91@gmail.com>
2012-12-04 16:57:08 +0000

    Remove Wordpress, new folder structures and flash message setup
    Signed-off-by: Chris Borrowdale <chrisborrowdale91@gmail.com>

---
+++ wordpress/header.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>121212 Concert</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+
+    <!--[if IE]>
+        <script type="text/javascript" src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <link rel="stylesheet" href="style.css" type="text/css" />
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+
+    <!--[if (gte IE 6)&(lte IE 8)]>
+        <script type="text/javascript" src="js/selectivizr.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+	<a href="#" class="sponsor-banner">
+		This is a sponsor banner
+	</a>
+	<div class="top">
+    	<div class="pre-header">
+	        <div class="wrapper cf">
+				<a href="#" class="btn donate">Donate</a>
+				<nav class="" role="navigation">
+					<ul class="nav">
+						<li><a href="#">#121212 Concert</a></li>
+						<li><a href="#">Watch on TV</a></li>
+						<li><a href="#">Social</a></li>
+						<li><a href="#">Shop</a></li>
+						<li><span class="separator">|</span></li>
+						<li class="share"><a href="#">Share</a></li>
+						<li class="share share-fb"><a href="#" class="icon-facebook"><span aria-hidden="true">Facebook</span></a></li>
+						<li class="share share-tw"><a href="#" class="icon-twitter"><span aria-hidden="true">Twitter</span></a></li>
+					</ul> <!-- .nav -->
+				</nav> <!-- g four-fifths -->
+	        </div> <!-- .wrapper -->
+	    </div> <!-- .pre-header -->
+		<div class="flash">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit Curabitur volutpat. Dolor sit amet, consectetur.</p>
+			<a class="close" href="#">&times;</a>
+		</div>
+		<div class="main" role="main">
+			<header role="header" class="table">
+				<div class="tr">
+					<h1 class="logo td">
+						<a href="/">
+							<img src="img/logo.png" alt="121212 Concert Logo" />
+						</a>
+					</h1>
+					<div class="td info">
+						<img src="img/desc.png" alt="The Concert for Sandy Relief to benefit The Robin Hood Relief Fund presented by Chase" />
+					</div> <!-- .td -->
+				</div> <!-- .tr -->
+			</header> <!-- .table -->
---
+++ wordpress/instagram.php
@@ -0,0 +1,13 @@
+<?php include 'header.php'; ?>
+	<h1 class="page-title">Instagram</h1>
+
+<ul id="js-instagram-all" class="instagram">
+						<?php for ($i=1; $i <= 9; $i++) { ?>
+							<li class="ig ig-<?=$i?>">
+								<a href="" class="insta-link"><img class="photo" src="" alt="" /></a>
+							</li>
+						<?php } ?>
+						</ul>
+
+</div>
+<?php include 'footer.php'; ?>
