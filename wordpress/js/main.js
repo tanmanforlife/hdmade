a4de1461123ec1ea3749a6f95a79b3d6c7f67d40
Chris Borrowdale <chrisborrowdale91@gmail.com>
2012-12-05 14:56:53 +0000

    Added fade out/fade in effects on Instagram feed
    Signed-off-by: Chris Borrowdale <chrisborrowdale91@gmail.com>

--- wordpress/js/main.js
+++ wordpress/js/main.js
@@ -287,7 +287,9 @@ $(window).load(function () {
                 var random = Math.floor(Math.random() * (max - min + 1)) + min;
                 var randomer = Math.floor(Math.random() * (maxer - min + 1)) + min;
 
-                $('ul#js-instagram li:nth-child(' + random + ') img.photo').attr('src', result[randomer]['standard_res']);
+                $('ul#js-instagram li:nth-child(' + random + ') img.photo').fadeOut(function() {
+                	$('ul#js-instagram li:nth-child(' + random + ') img.photo').attr('src', result[randomer]['standard_res']).fadeIn();
+            	});
 
             }
         });
