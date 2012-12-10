fb0a3d0955137b0bd614643c142d28a7eff4503c
Zach Ambrose <z@zambrose.com>
2012-12-10 18:40:10 -0500

    layout change to broadcast

--- broadcast.php
+++ broadcast.php
@@ -73,7 +73,7 @@
 
 
     <div class="pane-section middle-left">
-      <p><span style="color:#ffffff;"><span style="">Amctv.com<br>AOL<br>Clear Channel Media and Entertainment<br>Clear Channel Outdoor&nbsp;<br>Cookingchannel.com<br>Crackle<br>epixHD.com<br>EW.com<br>Foxmoviechannel.com<br>NBA.com</span></span></p>
+      <p><span style="color:#ffffff;"><span style="">Amctv.com<br>AOL<br>Clear Channel Media and Entertainment<br>Clear Channel Outdoor&nbsp;<br>Cookingchannel.com<br>Crackle<br>epixHD.com<br>EW.com<br>Foxmoviechannel.com</span></span></p>
     </div>
 
 
@@ -92,7 +92,7 @@
 
 
     <div class="pane-section middle-right">
-      <p><span style=""><span style="color: rgb(255, 255, 255);">People.com<br>Sundancechannel.com<br>Time.com<br>Vevo<br>VH1.com<br>Wetv.com<br>Yahoo<br>YouTube<br>VH1.com</span></span></p>
+      <p><span style=""><span style="color: rgb(255, 255, 255);">People.com<br>Sundancechannel.com<br>Time.com<br>Vevo<br>VH1.com<br>Wetv.com<br>Yahoo<br>YouTube<br>VH1.com<br>NBA.com</span></span></p>
     </div>
 
 
