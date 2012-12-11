ed0f6fcd5ffe2fae7fbc41824310caa86f8bd6af
Chris Borrowdale <chrisborrowdale91@gmail.com>
2012-12-11 11:05:23 +0000

    Changes to participate page to match PSD
    Signed-off-by: Chris Borrowdale <chrisborrowdale91@gmail.com>

--- social.php
+++ social.php
@@ -3,39 +3,42 @@
 <h1 class="page-title">Participate</h1>
 	<div class="participate cf">
 		<div class="one-fourth give-option">
-			<img src="img/ways-to-donate-text.png" alt="Donate via Text" />
-			<p>Text <strong>GIVE</strong> to 50555 to donate $10</p>
+			<figure><img src="img/donate-fb.png" alt="Donate via Facebook" /></figure>
+			<p>Buy your friend a 121212 gift and you will be making a $25 donation in honor of them</p>
 		</div> <!-- .text-to-give -->
+
 		<div class="one-fourth give-option">
-			<img src="img/ways-to-donate-amazon.png" alt="Donate via Amazon" />
-			<p>Donate using your Amazon account</p>
+			<figure><img src="img/donate-foursquare.png" alt="Checkin with Foursquare" /></figure>
+			<p>Be one of the first 25k to check-in anywhere using #121212concert and Samsung Galaxy will donate $10 to the Robin Hood Relief Fund on your behalf</p>
 		</div> <!-- .amazon -->
+
 		<div class="one-fourth give-option">
-			<img src="img/ways-to-donate-twitter.png" alt="Donate via Tweitter" />
-			<p>Tweet <strong>#givey #121212concert</strong> to donate</p>
+			<figure><img src="img/donate-glue.png" alt="Checkin with GetGlue" /></figure>
+			<p>Check-in during the concert and unlock your office 121212 Concert sticker</p>
 		</div> <!-- .tweet-to-give -->
+
 		<div class="one-fourth give-option">
-			<img src="img/ways-to-donate-fsq.png" alt="Donate via Foursquare" />
-			<p>Check in on foursquare and include <strong>#121212concert</strong> and Samsung will donate $2</p>
+			<figure><img src="img/donate-google.png" alt="Watch with Google Plus" /></figure>
+			<p>Watch the concert and join the conversation using #121212concert</p>
 		</div> <!-- .foursquare-to-give -->
+
 	</div> <!-- .table -->
 	<div class="participate cf">
 		<div class="one-fourth give-option">
-			<img src="img/ways-to-donate-text.png" alt="Donate via Text" />
+			<figure><img src="img/ways-to-donate-text.png" alt="Donate via Text" /></figure>
 			<p>Text <strong>GIVE</strong> to 50555 to donate $10</p>
 		</div> <!-- .text-to-give -->
+
 		<div class="one-fourth give-option">
-			<img src="img/ways-to-donate-amazon.png" alt="Donate via Amazon" />
-			<p>Donate using your Amazon account</p>
+			<figure><img src="img/donate-tumblr.png" alt="Tell us with Tumblr" /></figure>
+			<p>Tell us how you've been Robin Hood in your communities by tagging #Sandy #HowIHelped, and we'll feature posts on our Tumblr page </p>
 		</div> <!-- .amazon -->
+
 		<div class="one-fourth give-option">
-			<img src="img/ways-to-donate-twitter.png" alt="Donate via Tweitter" />
-			<p>Tweet <strong>#givey #121212concert</strong> to donate</p>
+			<figure><img src="img/donate-shazam.png" alt="Donate via Tweitter" /></figure>
+			<p>Shazam the 121212 concert and to donate</p>
 		</div> <!-- .tweet-to-give -->
-		<div class="one-fourth give-option">
-			<img src="img/ways-to-donate-fsq.png" alt="Donate via Foursquare" />
-			<p>Check in on foursquare and include <strong>#121212concert</strong> and Samsung will donate $2</p>
-		</div> <!-- .foursquare-to-give -->
+
 	</div> <!-- .table -->
 	</div>
 </div>
